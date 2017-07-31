bfdb40219b4c4559c60f509526cbe8f648f1a09d
Wladyslaw Lazin <wladislaw.lazin@gmail.com>
2017-07-31 19:47:22 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,12 @@
 package ru.job4j;
 
+/**
+ * Class Класс для вывода в консоль фразы "Hello World!"
+ * @author vlazin
+ * @since 31.07.2017
+ * @version 1	
+*/
+
 public class Calculate {
   public static void main(String[] args) {
     System.out.println("Hello world! I hope It isn't so boring for you :)");
