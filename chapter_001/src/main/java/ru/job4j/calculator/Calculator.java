e5f48415b52122613a3eb154836f7de04c37a8e5
Wladyslaw Lazin <wladislaw.lazin@gmail.com>
2017-08-12 11:22:41 +0300

    2.3. Элементарный калькулятор. [#185]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,4 +1,32 @@
 package ru.job4j.calculator;
 
+/**
+ * Elementary Calculator.
+ *
+ * @author Wladyslaw Lazin (wladislaw.lazin@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
 public class Calculator {
+    private double result;
+
+    public void add(double first, double second) {
+        this.result = first + second;
+    }
+
+    public void subtract(double first, double second) {
+        this.result = first - second;
+    }
+
+    public void div(double first, double second) {
+        this.result = first / second;
+    }
+
+    public void multiple(double first, double second) {
+        this.result = first * second;
+    }
+    public double getResult() {
+        return this.result;
+    }
 }
--- chapter_001/src/test/java/ru/job4j/calculator/package-info.java
+++ chapter_001/src/test/java/ru/job4j/calculator/package-info.java
@@ -1,1 +1,7 @@
+/**
+ * Package for elementary calculator test task.
+ * @author Wladyslaw Lazin (mailto:wladislaw.lazin@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 package ru.job4j.calculator;
