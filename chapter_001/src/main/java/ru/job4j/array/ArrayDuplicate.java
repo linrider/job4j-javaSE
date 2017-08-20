2ccd4485ecac1c1f671c4407efc0c62baf2bc976
Wladyslaw Lazin <wladislaw.lazin@gmail.com>
2017-08-20 09:13:55 +0300

    5.3. Удаление дубликатов в массиве. [#225]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -0,0 +1,40 @@
+package ru.job4j.array;
+/**
+ * ArrayDuplicate. task "5.3. Удаление дубликатов в массиве. [#225]".
+ *
+ * @author Wladyslaw Lazin (wladislaw.lazin@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+import java.util.Arrays;
+
+public class ArrayDuplicate {
+    /**
+     * Sort.
+     * @param array - array[].
+     */
+    public String[] remove(String[] array) {
+        //заменяем дубли на пустые элемнеты
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i].equals(array[j])) array[j] = "";
+            }
+        }
+        //двигаем пустые элементы в конец массива
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i; j < array.length; j++) {
+                if (array[i].equals("")) {
+                    array[i] = array[j];
+                    array[j] = "";
+                }
+            }
+        }
+        // находим количесво оригинальных элементов
+        int count = 0;
+        for (int i = 0; i < array.length; i++) if (!array[i].equals("")) count++;
+
+        String[] newArray = Arrays.copyOf(array, count);
+        return newArray;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+/**
+ * Test for ArrayDuplicate.
+ *
+ * @author Wladyslaw Lazin (wladislaw.lazin@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ArrayDuplicateTest {
+    @Test
+    public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
+        ArrayDuplicate arrayDuplicate = new ArrayDuplicate();
+        String[] testArray = {"cat", "dog", "mouse", "cat", "dog", "dog", "parrot", "chicken", "pig", "cow", "pig"};
+        String [] resultArray = arrayDuplicate.remove(testArray);
+        String[] expectedArray = {"cat", "dog", "mouse", "parrot", "chicken", "pig", "cow"};
+        assertThat(resultArray, is(expectedArray));
+
+    }
+}
