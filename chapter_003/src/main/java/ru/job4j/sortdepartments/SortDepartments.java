005fd30eea25d7d8e77f1623aab5c98f1afb3071
Wladyslaw Lazin <wladislaw.lazin@gmail.com>
2017-12-27 10:23:31 +0300

    in process

--- chapter_003/src/main/java/ru/job4j/sortdepartments/SortDepartments.java
+++ chapter_003/src/main/java/ru/job4j/sortdepartments/SortDepartments.java
@@ -10,7 +10,8 @@ public class SortDepartments {
             @Override
             public int compare(String left, String right) {
 
-                return left.length() > right.length() ? 1 : -1;
+                return left.charAt(1) > right.charAt(1) || left.length() >
+                        right.length() || left.charAt(left.length() - 1) > right.charAt(right.length() - 1) ? 1 : -1;
             }
         });
         return departments;
@@ -19,7 +20,18 @@ public class SortDepartments {
     public static void main(String[] args) {
         SortDepartments sortDepartments = new SortDepartments();
         ArrayList<String> list = new ArrayList<String>();
-        list.add("K1\Sk1");
+        list.add("K1\\SK1\\SSK1");
+        list.add("K1\\SK1");
+        list.add("K1\\SK2");
+        list.add("K1");
+        list.add("K2\\SK1");
+        list.add("K1\\SK1\\SSK2");
+        list.add("K2");
+        list.add("K2\\SK1\\SSK1");
+        list.add("K2\\SK1\\SSK2");
+
+        sortDepartments.sortAscending(list);
+        System.out.println(list);
 
 
     }
