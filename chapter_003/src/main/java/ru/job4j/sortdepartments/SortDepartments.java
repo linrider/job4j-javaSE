6253879ff2bac52e0227352ebffc4f8a383f5c40
Wladyslaw Lazin <wladislaw.lazin@gmail.com>
2017-12-28 19:39:00 +0300

    in process

--- chapter_003/src/main/java/ru/job4j/sortdepartments/SortDepartments.java
+++ chapter_003/src/main/java/ru/job4j/sortdepartments/SortDepartments.java
@@ -5,13 +5,15 @@ import java.util.Comparator;
 import java.util.List;
 
 public class SortDepartments {
+    private static final int[] pos = {1, 5, 10};
+
     public List<String> sortAscending(List<String> departments) {
         departments.sort(new Comparator<String>() {
             @Override
             public int compare(String left, String right) {
-
-                return left.charAt(1) > right.charAt(1) || left.length() >
-                        right.length() || left.charAt(left.length() - 1) > right.charAt(right.length() - 1) ? 1 : -1;
+                int res = ((left.length() == 2) || (right.length() == 2)) ? Integer.compare((int) left.charAt(pos[0]), (int) right.charAt(pos[0])) : 1;
+                //res = ((left.length() == 6) || (right.length() == 6)) ? Integer.compare((int) left.charAt(pos[1]), (int) right.charAt(pos[1])) : 1;
+                return res;
             }
         });
         return departments;
@@ -20,18 +22,25 @@ public class SortDepartments {
     public static void main(String[] args) {
         SortDepartments sortDepartments = new SortDepartments();
         ArrayList<String> list = new ArrayList<String>();
-        list.add("K1\\SK1\\SSK1");
         list.add("K1\\SK1");
+        list.add("K2");
         list.add("K1\\SK2");
         list.add("K1");
         list.add("K2\\SK1");
         list.add("K1\\SK1\\SSK2");
-        list.add("K2");
+
         list.add("K2\\SK1\\SSK1");
         list.add("K2\\SK1\\SSK2");
+        list.add("K1\\SK1\\SSK1");
 
+        for (String x : list) {
+            System.out.println(x);
+        }
+        System.out.println("-------------");
         sortDepartments.sortAscending(list);
-        System.out.println(list);
+        for (String x : list) {
+            System.out.println(x);
+        }
 
 
     }
